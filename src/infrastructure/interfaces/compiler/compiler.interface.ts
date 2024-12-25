75b4a649ea47f5867db4572139c98b3e03c2108c
IgorRusskikh <russkikh200@mail.ru>
2024-12-26 01:07:55 +0700

    feat: add new method and option

--- src/infrastructure/interfaces/compiler/compiler.interface.ts
+++ src/infrastructure/interfaces/compiler/compiler.interface.ts
@@ -1,4 +1,7 @@
 export default interface Compiler {
+  contractDir: string;
+
+  format(sourceCode: string): Promise<string>;
   compile(sourceCode: string): Promise<string>;
   validate(sourceCode: string): Promise<boolean>;
 }
