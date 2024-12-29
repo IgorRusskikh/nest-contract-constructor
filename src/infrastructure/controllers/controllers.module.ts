59c14916eb7d81f8417d093088e6e60055eed740
IgorRusskikh <russkikh200@mail.ru>
2024-12-30 01:06:05 +0700

    feat: add service in module dependencies

--- src/infrastructure/controllers/controllers.module.ts
+++ src/infrastructure/controllers/controllers.module.ts
@@ -2,9 +2,10 @@ import { CompilerController } from './contract/contract.controller';
 import { CompilerERC20Service } from 'src/infrastructure/services/compiler-erc20/compiler-erc20.service';
 import { ETHCompiler } from 'src/usecases/eth-compiler/compiler.service';
 import { Module } from '@nestjs/common';
+import { TestsERC20 } from '../services/tests-erc20/tests-erc20.service';
 
 @Module({
   controllers: [CompilerController],
-  providers: [CompilerERC20Service, ETHCompiler],
+  providers: [ETHCompiler, CompilerERC20Service, TestsERC20],
 })
 export class ControllersModule {}
