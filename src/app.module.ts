7121a5fe2f4f039c52c26a26e8130d265a0c673b
IgorRusskikh <russkikh200@mail.ru>
2024-12-30 01:06:35 +0700

    feat: delete app service cuz of useless

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { AppService } from './app.service';
 import { ControllersModule } from './infrastructure/controllers/controllers.module';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [ControllersModule],
   controllers: [],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
