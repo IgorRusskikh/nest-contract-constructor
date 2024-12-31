1133a684527449b9c24fb673165a085699c84c7e
IgorRusskikh <russkikh200@mail.ru>
2024-12-31 20:13:58 +0700

    feat: renamed methods and property, method deploy removed in other class

--- src/infrastructure/abstracts/abstract-eth-tests/abstract-eth-tests.ts
+++ src/infrastructure/abstracts/abstract-eth-tests/abstract-eth-tests.ts
@@ -1,41 +1,39 @@
-import { JsonRpcProvider, Wallet } from 'ethers';
+import { BaseContract, JsonRpcProvider, Wallet } from 'ethers';
 
 import { BadRequestException } from '@nestjs/common';
-import TestETHDto from 'src/infrastructure/dtos/test-eth/test-eth.dto';
+import ETHTest from 'src/infrastructure/interfaces/test-eth/test-eth.interface';
 
-export abstract class BaseETHTests {
+export abstract class BaseETHTests implements ETHTest {
   hardhatServer = `http://127.0.0.1:8545/`;
 
   readonly provider: JsonRpcProvider;
-  private wallet: Wallet;
+  private owner: Wallet;
+
+  protected deployedContract: BaseContract | any;
 
   constructor() {
     this.provider = new JsonRpcProvider(this.hardhatServer);
-    this.wallet = new Wallet(
+    this.owner = new Wallet(
       '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
       this.provider,
     );
   }
 
-  getWallet() {
-    return this.wallet;
+  getOwner() {
+    return this.owner;
   }
 
-  setWallet(privateKey: string) {
+  setOwner(privateKey: string) {
     if (!privateKey) {
       return 'Private key is necessary';
     }
 
     try {
-      this.wallet = new Wallet(privateKey, this.provider);
+      this.owner = new Wallet(privateKey, this.provider);
     } catch (err) {
       console.log(err);
       throw new BadRequestException(err.message);
     }
-    return this.wallet;
-  }
-
-  async deploy(deployData: TestETHDto): Promise<boolean> {
-    return true;
+    return this.owner;
   }
 }
