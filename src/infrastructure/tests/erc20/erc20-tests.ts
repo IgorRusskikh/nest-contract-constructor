dcaaf4b282efb88e0ba3e94fb2ce67f0c18335b1
IgorRusskikh <russkikh200@mail.ru>
2025-01-04 14:44:05 +0700

    feat: add class to control tests for contracts

---
+++ src/infrastructure/tests/erc20/erc20-tests.ts
@@ -0,0 +1,16 @@
+import { BaseETHTests } from 'src/infrastructure/abstracts/abstract-eth-tests/abstract-eth-tests';
+import { ERC20DeploymentTests } from './implementations/erc20-deployment-tests';
+import { ERC20TransactionTests } from './implementations/erc20-transaction-tests';
+import TestETHDto from 'src/infrastructure/dtos/test-eth/test-eth.dto';
+
+export class TestERC20Service extends BaseETHTests {
+  addMinimalTests(testERC20Dto: TestETHDto) {
+    const deployment = new ERC20DeploymentTests();
+    const transactions = new ERC20TransactionTests();
+
+    this.addTest([
+      deployment.testDeployment.bind(this, testERC20Dto),
+      transactions.checkOwnerBalance,
+    ]);
+  }
+}
