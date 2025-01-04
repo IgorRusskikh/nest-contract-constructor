8ac2e0aa11e3ae4ddd458230af5e608cbe0061e9
IgorRusskikh <russkikh200@mail.ru>
2025-01-04 14:43:35 +0700

    feat: add all dependencies for both controllers

--- src/infrastructure/controllers/controllers.module.ts
+++ src/infrastructure/controllers/controllers.module.ts
@@ -1,11 +1,21 @@
-import { CompilerController } from './contract/contract.controller';
+import { BaseETHTests } from '../abstracts/abstract-eth-tests/abstract-eth-tests';
 import { CompilerERC20Service } from 'src/infrastructure/services/compiler-erc20/compiler-erc20.service';
+import { ContractCompilerController } from './contract-compile/contract-compile.controller';
+import { ContractTestController } from './contract-test/contract-test.controller';
+import { ERC20DeploymentTests } from '../tests/erc20/implementations/erc20-deployment-tests';
+import { ERC20TransactionTests } from '../tests/erc20/implementations/erc20-transaction-tests';
 import { ETHCompiler } from 'src/usecases/eth-compiler/compiler.service';
+import { ETHTest } from 'src/usecases/eth-test/eth-test.service';
 import { Module } from '@nestjs/common';
-import { TestsERC20 } from '../services/tests-erc20/tests-erc20.service';
 
 @Module({
-  controllers: [CompilerController],
-  providers: [ETHCompiler, CompilerERC20Service, TestsERC20],
+  controllers: [ContractCompilerController, ContractTestController],
+  providers: [
+    ETHCompiler,
+    CompilerERC20Service,
+    ETHTest,
+    ERC20DeploymentTests,
+    ERC20TransactionTests,
+  ],
 })
 export class ControllersModule {}
