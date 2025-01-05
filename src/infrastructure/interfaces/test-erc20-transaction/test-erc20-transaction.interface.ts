f8ca76376725960f7500582d13bb6cf94ff7d672
IgorRusskikh <russkikh200@mail.ru>
2025-01-05 18:10:36 +0700

    feat: add methods args

--- src/infrastructure/interfaces/test-erc20-transaction/test-erc20-transaction.interface.ts
+++ src/infrastructure/interfaces/test-erc20-transaction/test-erc20-transaction.interface.ts
@@ -2,9 +2,11 @@ import IETHTest from '../test-eth/test-eth.interface';
 import { TestERC20Dto } from 'src/infrastructure/dtos/test-erc20/test-erc20.dto';
 
 export default interface IERC20TransactionTests extends IETHTest {
+  isApproved: boolean;
+
   checkOwnerBalance(): Promise<boolean>;
   checkTotalSupply(dto: TestERC20Dto): Promise<boolean>;
-  testApprove(): Promise<boolean>;
-  testTransfer(): Promise<boolean>;
-  testTransferFrom(): Promise<boolean>;
+  testApprove(dto: TestERC20Dto): Promise<boolean>;
+  testTransfer(dto: TestERC20Dto): Promise<boolean>;
+  testTransferFrom(dto: TestERC20Dto): Promise<boolean>;
 }
--- src/infrastructure/tests/erc20/erc20-tests.ts
+++ src/infrastructure/tests/erc20/erc20-tests.ts
@@ -4,7 +4,7 @@ import { ERC20TransactionTests } from './implementations/erc20-transaction-tests
 import TestETHDto from 'src/infrastructure/dtos/test-eth/test-eth.dto';
 
 export class TestERC20Service extends BaseETHTests {
-  addMinimalTests(testERC20Dto: TestETHDto) {
+  async addMinimalTests(testERC20Dto: TestETHDto) {
     const deployment = new ERC20DeploymentTests();
     const transactions = new ERC20TransactionTests();
 
@@ -12,6 +12,9 @@ export class TestERC20Service extends BaseETHTests {
       deployment.testDeployment.bind(this, testERC20Dto),
       transactions.checkOwnerBalance.bind(this),
       transactions.checkTotalSupply.bind(this, testERC20Dto),
+      transactions.testApprove.bind(this, testERC20Dto),
+      transactions.testTransfer.bind(this, testERC20Dto),
+      transactions.testTransferFrom.bind(this, testERC20Dto),
     ]);
   }
 }
