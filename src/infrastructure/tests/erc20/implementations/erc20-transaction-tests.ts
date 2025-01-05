c9c9bf5c18f6e63fa134b5227c87a19ab61f7cfb
IgorRusskikh <russkikh200@mail.ru>
2025-01-05 18:11:33 +0700

    feat: realized all basic tests for erc20 contract

--- src/infrastructure/tests/erc20/implementations/erc20-transaction-tests.ts
+++ src/infrastructure/tests/erc20/implementations/erc20-transaction-tests.ts
@@ -1,19 +1,25 @@
+import { formatUnits, parseUnits } from 'ethers';
+
 import { BaseETHTests } from 'src/infrastructure/abstracts/abstract-eth-tests/abstract-eth-tests';
 import IERC20TransactionTests from 'src/infrastructure/interfaces/test-erc20-transaction/test-erc20-transaction.interface';
 import { TestERC20Dto } from 'src/infrastructure/dtos/test-erc20/test-erc20.dto';
-import ethers from 'ethers';
 
 export class ERC20TransactionTests
   extends BaseETHTests
   implements IERC20TransactionTests
 {
+  isApproved: boolean;
+
   async checkOwnerBalance(): Promise<boolean> {
     try {
       if (!this.deployedContract) {
-        // TODO: ADD RETURN MESSAGE THAT NEED TO DEPLOY FIRSTLY
+        console.log('Contract needs to be deployed first.');
+        return false;
       }
 
-      const balance = this.deployedContract.balanceOf(this.getOwner());
+      const balance = await this.deployedContract.balanceOf(
+        this.getOwner().address,
+      );
       const totalSupply = await this.deployedContract.totalSupply();
 
       if (balance === totalSupply) {
@@ -21,12 +27,12 @@ export class ERC20TransactionTests
         return true;
       } else {
         console.log(
-          `Owner balance is incorrect. Expected: ${totalSupply.toString()}, Got: ${balance.toString()}`,
+          `[TEST ERROR] Owner balance is incorrect. Expected: ${totalSupply.toString()}, Got: ${balance.toString()}`,
         );
         return false;
       }
     } catch (err) {
-      console.log(`Error checking owner balance: ${err}`);
+      console.log(`[TEST ERROR] Error checking owner balance: ${err}`);
       return false;
     }
   }
@@ -37,7 +43,8 @@ export class ERC20TransactionTests
   }: TestERC20Dto): Promise<boolean> {
     try {
       if (!this.deployedContract) {
-        // TODO: ADD RETURN MESSAGE THAT NEED TO DEPLOY FIRSTLY
+        console.log('Contract needs to be deployed first.');
+        return false;
       }
 
       const contractTotalSupply = BigInt(
@@ -47,30 +54,153 @@ export class ERC20TransactionTests
 
       if (contractTotalSupply !== calcSupply) {
         console.log(
-          `Contract total supply (${contractTotalSupply}) isn't equal user total supply ${calcSupply}`,
+          `[TEST ERROR] Contract total supply (${contractTotalSupply}) isn't equal to user total supply (${calcSupply})`,
         );
         return false;
       }
 
       return true;
     } catch (err) {
-      console.log(`Error checking total supply: ${err}`);
+      console.log(`[TEST ERROR] Error checking total supply: ${err}`);
       return false;
     }
   }
 
-  async testApprove(): Promise<boolean> {
-    // Ваша логика для тестирования approve
-    return true; // или false в зависимости от результата
+  async testApprove({ decimals }: TestERC20Dto): Promise<boolean> {
+    try {
+      if (!this.deployedContract) {
+        console.log('Contract needs to be deployed first.');
+        return false;
+      }
+
+      const approveAmount = parseUnits('50', decimals);
+
+      const approveTx = await this.deployedContract.approve(
+        this.getSpender().address,
+        BigInt(approveAmount),
+      );
+
+      await approveTx.wait();
+
+      const approvedAmount = await this.deployedContract.allowance(
+        this.getOwner().address,
+        this.getSpender().address,
+      );
+
+      if (BigInt(approvedAmount) !== approveAmount) {
+        console.log(
+          `[TEST ERROR] Spender approved amount isn't equal target amount: ${approvedAmount} !== ${approveAmount}`,
+        );
+        return false;
+      }
+
+      return true;
+    } catch (err) {
+      console.log(`[TEST ERROR] Error checking approve: ${err}`);
+      return false;
+    }
   }
 
-  async testTransfer(): Promise<boolean> {
-    // Ваша логика для тестирования transfer
-    return true; // или false в зависимости от результата
+  async testTransfer({ decimals }: TestERC20Dto): Promise<boolean> {
+    try {
+      const ownerBalanceBefore = await this.deployedContract.balanceOf(
+        this.getOwner().address,
+      );
+      const spenderBalanceBefore = await this.deployedContract.balanceOf(
+        this.getSpender().address,
+      );
+
+      const transferAmount = parseUnits('50', decimals);
+      const transferTx = await this.deployedContract.transfer(
+        this.getSpender().address,
+        transferAmount,
+      );
+
+      await transferTx.wait();
+
+      const ownerBalanceAfter = await this.deployedContract.balanceOf(
+        this.getOwner().address,
+      );
+      const spenderBalanceAfter = await this.deployedContract.balanceOf(
+        this.getSpender().address,
+      );
+
+      if (
+        ownerBalanceAfter === ownerBalanceBefore - transferAmount &&
+        spenderBalanceAfter === spenderBalanceBefore + transferAmount
+      ) {
+        return true;
+      } else {
+        console.log("[TEST ERROR] Balances of wallets isn't equals");
+        return false;
+      }
+    } catch (err) {
+      console.log(`[TEST ERROR] Error checking transfer: ${err}`);
+      return false;
+    }
   }
 
-  async testTransferFrom(): Promise<boolean> {
-    // Ваша логика для тестирования transferFrom
-    return true; // или false в зависимости от результата
+  async testTransferFrom({ decimals }: TestERC20Dto): Promise<boolean> {
+    try {
+      const ownerAddress = this.getOwner().address;
+      const spenderAddress = this.getSpender().address;
+      const tokensAmount = parseUnits('25', decimals);
+
+      const ownerBalance = await this.deployedContract.balanceOf(ownerAddress);
+      if (ownerBalance < tokensAmount) {
+        console.log(
+          `Insufficient balance: ${formatUnits(ownerBalance, decimals)}`,
+        );
+        return false;
+      }
+
+      const approveTx = await this.deployedContract.approve(
+        spenderAddress,
+        tokensAmount,
+      );
+      await approveTx.wait();
+
+      const allowance = await this.deployedContract.allowance(
+        ownerAddress,
+        spenderAddress,
+      );
+
+      if (allowance < tokensAmount) {
+        console.log(
+          `[TEST ERROR] Insufficient allowance: ${formatUnits(allowance, decimals)}`,
+        );
+        return false;
+      }
+
+      const spenderBalanceBefore = await this.deployedContract.balanceOf(
+        this.getSpender(),
+      );
+
+      const transferFromTx = await this.deployedContract
+        .connect(this.getSpender())
+        .transferFrom(ownerAddress, spenderAddress, tokensAmount);
+
+      await transferFromTx.wait();
+
+      const ownerBalanceAfter =
+        await this.deployedContract.balanceOf(ownerAddress);
+      const spenderBalanceAfter =
+        await this.deployedContract.balanceOf(spenderAddress);
+
+      if (
+        ownerBalanceAfter === ownerBalance - tokensAmount &&
+        spenderBalanceAfter === spenderBalanceBefore + tokensAmount
+      ) {
+        return true;
+      } else {
+        console.log('[TEST ERROR] Balances did not update correctly.');
+        return false;
+      }
+    } catch (err) {
+      console.log(
+        `[TEST ERROR] Error details: ${JSON.stringify(err, null, 2)}`,
+      );
+      return false;
+    }
   }
 }
